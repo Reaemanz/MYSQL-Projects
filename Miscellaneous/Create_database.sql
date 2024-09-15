c498b9751126e2ad31613fb55a4a02b132ece578
Nsubuga Emmanuel Reagan <nsubugaemmanuel1@gmail.com>
2024-09-15 16:00:19 +0300

    Add files via upload

---
+++ Miscellaneous/Create_database.sql
@@ -0,0 +1,10 @@
+-- CREATING A DATABASE
+
+CREATE DATABASE Emmanuel;
+
+USE emmanuel;
+
+CREATE table Salary
+  (Name_id     INT         PRIMARY KEY    auto_increment,
+   VENDOR_id   int         NOT NULL,
+   City        varchar(50) not null);
---
+++ Miscellaneous/Project_1_Where Clause.sql
@@ -0,0 +1,7 @@
+USE ap;
+
+-- USING THE WHERE CLAUSE
+SELECT vendor_city, vendor_state
+FROM vendors
+WHERE vendor_city = 'Washington' AND vendor_state = 'DC';
+
